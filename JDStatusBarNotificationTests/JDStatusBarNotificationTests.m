a79055782ae1d3cfa1aed562eb818b53aefde41b
andelf <andelf@gmail.com>
2015-03-19 23:59:43 +0800

    add projectfile

---
+++ JDStatusBarNotificationTests/JDStatusBarNotificationTests.m
@@ -0,0 +1,40 @@
+//
+//  JDStatusBarNotificationTests.m
+//  JDStatusBarNotificationTests
+//
+//  Created by 王淑羽 on 3/19/15.
+//  Copyright (c) 2015 Feather Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface JDStatusBarNotificationTests : XCTestCase
+
+@end
+
+@implementation JDStatusBarNotificationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
